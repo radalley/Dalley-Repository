cd605249042644d10e8b508ec026f56fe8b3e1a9
Riley <rdalley@mail.bradley.edu>
2020-10-18 19:26:47 -0500

    opaaaa

---
+++ PennyPixel_2DTilemapProject/A5 - 3D/Assets/Scripts/EndTriggerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTriggerScript : MonoBehaviour
+{
+    private ScoreManager scoreManagerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreManagerScript = GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<ScoreManager>();
+
+    }
+
+    void OnTriggerEnter(Collider theCollider)
+    {
+        if (theCollider.CompareTag("Player"))
+        {
+            
+            scoreManagerScript.win=1;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PennyPixel_2DTilemapProject/A5 - 3D/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text textbox;
+    public int win = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textbox = GetComponent<Text>();
+        textbox.text = "MaKe It To The End To Win:";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (win == 1)
+        {
+            textbox.text = "You Win! Press R to try again!";
+        }
+        //Press R to restart if game is over
+       
+
+    }
+}
